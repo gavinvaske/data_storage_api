ff0a80478eb3d01b5bc00f408c81b9663fa6ba56
Gavin Vaske <Gavin.Vaske@gmail.com>
2022-06-20 18:51:20 -0500

    feature: protect endpoints using okta authorization token

--- application/index.js
+++ application/index.js
@@ -1,10 +1,12 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const authMiddleware = require('./middleware/auth')
 
 const defaultPort = 8080;
 const PORT = process.env.PORT || defaultPort;
 
 const app = express();
+app.use(authMiddleware);
 app.use('/', require('./controllers/index'));
 
 app.listen(PORT, () => {
